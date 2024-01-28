9132fa6ba2dbc304ff9f984b15503e54bcaa2322
Joao-Vitor-Lopes <joao.lopes200@etec.sp.gov.br>
2024-01-28 03:17:41 -0300

    carrossel consertado

--- js/projects.js
+++ js/projects.js
@@ -14,6 +14,23 @@ function nextImage(){
     }
 
 
-    document.getElementById("radio" + count).checked = true;
+    document.getElementById("radio"+count).checked = true;
 }
 
+(function() {
+    let count = 1;
+    document.getElementById("radio1-custom").checked = true;
+
+    setInterval(function() {
+        nextImage();
+    }, 3500);
+
+    function nextImage() {
+        count++;
+        if (count > 3) {
+            count = 1;
+        }
+
+        document.getElementById("radio" + count + "-custom").checked = true;
+    }
+})();
